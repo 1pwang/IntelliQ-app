a9bbac775ac10a7cf6f0949dd15aff33a4ebd0d6
Pauline Wangunyu <paulinewangunyu@Paulines-Air.rocket-fibre.home>
2024-05-17 22:10:35 +0100

    Implement home page to redirect user to after signup

--- app/intelliQ/src/components/LoginForm.vue
+++ app/intelliQ/src/components/LoginForm.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" @submit.prevent="handleSignUp">
+      <form class="space-y-6" @submit="handleSignUp">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
@@ -42,6 +42,7 @@
 <script setup lang="ts">
 import { ref, getCurrentInstance } from 'vue'
 import {UserService} from "@/services/userService";
+import router from "@/router";
 
 const email = ref('');
 const password = ref('');
@@ -54,6 +55,8 @@ const handleSignUp = async () => {
   try {
     const response = await userService.signUp(email.value, password.value);
     console.log('Login successful:', response);
+    await router.push('/home');
+
   } catch (err) {
     error.value = 'Login failed. Please check your credentials.';
   }
